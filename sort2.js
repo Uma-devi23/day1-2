51a9ba02f48dbc711dc44211f36b3db6703b41fa
uma_18 <124366056+Uma-devi23@users.noreply.github.com>
2023-07-18 15:23:32 +0530

    Add files via upload

---
+++ sort2.js
@@ -0,0 +1,51 @@
+function compareValues(a, b) {
+    if (typeof a === "number" && typeof b === "number") {
+      return a-b;
+    } else if (typeof a === "number") {
+      return -1;
+    } else if (typeof b === "number") {
+      return 1;
+    } else {
+      return a.localCompare(b);
+    }
+  }
+  
+  function sortNumbersAndCharacters(arr) {
+ 
+    let numbers = arr.filter((item) => typeof item === "number");
+    let Characters = arr.filter((item) => typeof item === "string");
+  
+    numbers.sort((a,b) => a-b);
+    Characters.sort();
+  
+    let sortedArr = [];
+    let numIndex = 0;
+    let charIndex = 0;
+  
+    for (let item of arr) {
+      if (typeof item === "number") {
+        sortedArr.push(numbers[numIndex]);
+        numIndex++;
+      } else {
+        sortedArr.push(Characters[charIndex]);
+        charIndex++;
+      }
+    }
+  
+    return sortedArr;
+  }
+  
+  function numbersThenCharacters(inputArray) {
+
+    return inputArray.map((subArray) => sortNumbersAndCharacters(subArray));
+  }
+  
+  const input = [
+    [11, 12, 24, 23, "a", "b"],
+    [56, "c", 45],
+    [67, "d"],
+    ["f", "e", 78]
+  ];
+  
+  const output = numbersThenCharacters(input);
+  console.log(output);
